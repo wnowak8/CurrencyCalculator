4103618c37906f26e0dc703081d0552be5aa9c32
wnowak9 <wiktoria.nowak@aigorithmics.com>
2023-12-05 11:04:15 +0100

    Change key in db

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -2,5 +2,5 @@ CREATE TABLE public."exchange_rates" (
 	"date" date NOT NULL,
     "currency" char(3) NOT NULL,
    	"value" float8 NOT NULL,
-	CONSTRAINT "exchange_rates_pkey" PRIMARY KEY ("date","currency")
+	CONSTRAINT "exchange_rates_pkey" PRIMARY KEY ("currency")
 );
